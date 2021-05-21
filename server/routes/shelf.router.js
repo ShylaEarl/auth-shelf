eca806d100e5d14cc59bd4f8860861f0f68bebcc
Shyla Earl <shyla.earl@gmail.com>
2021-05-21 11:16:48 -0500

    get, post functional and appending to DOM, delete button set up

--- server/routes/shelf.router.js
+++ server/routes/shelf.router.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
-// const {
-//   rejectUnauthenticated,
-// } = require('../modules/authentication-middleware');
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
 /**
  * Get all of the items on the shelf
  */
 router.get('/', (req, res) => {
-  res.sendStatus(200); // For testing only, can be removed
+  // res.sendStatus(200); // For testing only, can be removed
   const queryText = `SELECT * FROM item;`;
   pool.query(queryText)
     .then( result => {
@@ -25,11 +25,12 @@ router.get('/', (req, res) => {
 /**
  * Add an item for the logged in user to the shelf
  */
-router.post('/', (req, res) => {
+router.post('/', rejectUnauthenticated, (req, res) => {
   // endpoint functionality
+  const user_id = req.user.id;
   const query  = `INSERT INTO "item" ("description", "image_url", "user_id")
       VALUES ($1, $2, $3)`;
-  pool.query(query, [req.body.description, req.body.image_url, req.body.user_id])
+  pool.query(query, [req.body.description, req.body.image_url, user_id])
     .then(result => {
       console.log('item object POST', result.rows);
       res.sendStatus(201);
@@ -42,7 +43,7 @@ router.post('/', (req, res) => {
 /**
  * Delete an item if it's something the logged in user added
  */
-router.delete('/:id',rejectUnauthenticated, (req, res) => {
+router.delete('/:id', rejectUnauthenticated, (req, res) => {
   // endpoint functionality
   
     let reqId = req.params.id;
